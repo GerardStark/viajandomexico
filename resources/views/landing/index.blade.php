02d13314d27067f465549c7efb125277ad5d7edf
Iorchko <snotty_315@hotmail.com>
2016-03-11 15:13:03 -0600

    pi pa pu
    xD

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -141,7 +141,7 @@
 
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog"role="document">
         <div class="modal-lg">
 
                      <div id="canvas">
